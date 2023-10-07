62c8ee8051cae8645074ab9473c3f864bcf7db7e
Tedz517 <135931110+Tedz517@users.noreply.github.com>
2023-10-07 23:19:38 +0700

    Add files via upload

---
+++ Konvers_suhu.php
@@ -0,0 +1,5 @@
+<?php
+$celcius = 25;
+$fahrenheit = ($celcius * 9 / 5) + 32;
+
+echo "Suhu $celcius Celcius sama dengan $fahrenheit Fahrenheit.";
---
+++ Konvers_uang.php
@@ -0,0 +1,7 @@
+<?php
+$kurs_usd_idr = 15000;
+$jumlah_usd = 50;
+
+$jumlah_idr = $jumlah_usd * $kurs_usd_idr;
+
+echo "$jumlah_usd Dolar Amerika setara dengan Rp $jumlah_idr Rupiah Indonesia.";
